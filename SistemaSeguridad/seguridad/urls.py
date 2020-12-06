476def1f533cfea84fff62fb9f82f412fc0bc80b
Denisse <denisse.hunt28@gmail.com>
2020-12-05 20:01:53 -0600

    Actualización nueva versión (Abner)

---
+++ SistemaSeguridad/seguridad/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+
+
+from .views import index, login, forgot
+
+
+urlpatterns = [
+    path('', index),
+]
+
+seguridad_patterns =[
+	path('login.html/',login,name='login'),
+	path('forgot-password.html/',forgot,name='forgot')
+
+
+]
---
+++ SistemaSeguridad/seguridad/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+#Vista de la pagina principal
+
+def index(request):
+	return render(request,"seguridad/index.html")
+
+def login(request):
+	return render(request,"seguridad/login.html")
+
+def forgot(request):
+	return render(request,"seguridad/forgot-password.html")
+
+
+	
