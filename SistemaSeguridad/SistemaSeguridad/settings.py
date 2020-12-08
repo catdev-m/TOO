5156207023b82161e2976223ea40a82cd1c9f611
DESKTOP-81RD0UR\Luis Duque <md17007@ues.edu.sv>
2020-12-07 19:41:49 -0600

    cambios para que pueda enviar correos, falta la lógica de estar verificado el correo, y el cuerpo del correo

--- SistemaSeguridad/SistemaSeguridad/settings.py
+++ SistemaSeguridad/SistemaSeguridad/settings.py
@@ -123,3 +123,9 @@ LOGOUT_REDIRECT_URL = 'login'
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST="smtp.gmail.com"
+EMAIL_USE_TLS=True
+EMAIL_PORT=587
+EMAIL_HOST_USER="csystem205@gmail.com"
+EMAIL_HOST_PASSWORD="alveger2020"
--- SistemaSeguridad/seguridad/views.py
+++ SistemaSeguridad/seguridad/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate
+from django.core.mail import send_mail
+from django.conf import settings
 # Create your views here.
 
 #Vista de la pagina principal
@@ -14,6 +16,13 @@ def login(request):
 
 
 def forgot(request):
+	if request.method=="POST":
+		subject=request.POST["correo"]
+		email_from=settings.EMAIL_HOST_USER
+		recipient_list=["csystem205@gmail.com",subject]
+		send_mail('Recupere su contraseña','Recupere su contraseña ',email_from,recipient_list)
+		
+		return render(request,"seguridad/forgot-password.html")
 	return render(request,"seguridad/forgot-password.html")
 
 def primer(request):
